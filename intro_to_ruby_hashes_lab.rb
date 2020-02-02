fe9847fb865a7c5315820105b78a6e41736fd374
Claudia Pierson <c172@umbc.edu>
2020-02-02 06:25:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,14 +27,12 @@ def monopoly_with_second_tier
 	   
 	 },
 	   :names => {}
-	   
-	 
 	
-	
+	}	
 	
 }
 end
-end
+
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
 
